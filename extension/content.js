eca656e16624fff7f57bf2586c038ecc217c0a88
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-01-12 22:11:29 +0530

    feat : extension only loaded on youtube.com

--- extension/content.js
+++ extension/content.js
@@ -1,15 +1,10 @@
+console.log("WE ARE INSIDE YOUTUBE")
 let timer;
 
 timer = setInterval(() => {
     if(location.href.includes('youtube.com/shorts')){
         location.href = "https://namastedev.com/learn"
-    } else if(!location.href.includes('youtube')) {
-        console.log("I dont know what you are doing")
-        clearInterval(timer)
-    }
-
-
-    
+    } 
 } , 3000)
 
   
